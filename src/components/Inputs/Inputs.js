01a6f73e8ce439d6de380cdbebbf3d4b10d44ff8
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-02-13 09:41:56 -0300

    se factoriza funcion de editar y se agrega reset para vaciar la tabla

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'; 
+import React, {useState, useRef} from 'react'; 
 import './inputs.scss'
 
 const Inputs = ({newProductos, productos}) => {
@@ -31,35 +31,33 @@ const Inputs = ({newProductos, productos}) => {
     //     setPrice(event.target.value);
     // }
 
+    const refForm = useRef();
+
     const [data, setData] = useState({
         id: null,
         name: null,
         make: null,
-        price: null,
+        price: null
     });
 
     const setDataInput = event => {
         const { name , value } = event.target;
-        console.log(name, value, {...data, [name]: value});
+        // console.log(name, value, {...data, [name]: value});
         setData({...data, [name]: value});
     };
 
     const addProducts = () => {
         newProductos([...productos, data]);
+        refForm.current.reset();
     }
     
 
     const editProducts = ()=> { 
-        const editData = {
-            id: data.id,
-            name: data.name,
-            make: data.make,
-            price: data.price
-        }
         const arr = [...productos];
         const index = arr.findIndex(producto => producto.id == data.id);
-        arr.splice(index, 1, editData);
+        arr.splice(index, 1, data);
         newProductos(arr);
+        refForm.current.reset();
     }
 
 
@@ -67,16 +65,17 @@ const Inputs = ({newProductos, productos}) => {
     return (
         
         <div className="box-add">
-            <h2> Add && Edit </h2>
-                <div className="space-add">
-                    <input id ="inputId" name="id" className ="input-id" onChange={setDataInput} placeholder ="id"></input>
-                    <input id = "inputName" name="name" className ="input-name" onChange={setDataInput} placeholder ="name"></input>
-                    <input id = "inputMake" name="make" className ="input-make" onChange={setDataInput} placeholder ="make"></input>
-                    <input id = "inputPrice" name="price" className ="input-price" onChange={setDataInput} placeholder ="price"></input>
-                </div>
-            <button className="add" onClick ={addProducts}>ADD</button>
-            <button className="edit" onClick ={editProducts}>EDIT</button>
-           
+            <form ref={refForm} onSubmit={e => e.preventDefault()}>
+                <h2> Add && Edit </h2>
+                    <div className="space-add">
+                        <input id="inputId" name="id" className ="input-id" onChange={setDataInput} placeholder ="id"></input>
+                        <input id="inputName" name="name" className ="input-name" onChange={setDataInput} placeholder ="name"></input>
+                        <input id="inputMake" name="make" className ="input-make" onChange={setDataInput} placeholder ="make"></input>
+                        <input id="inputPrice" name="price" className ="input-price" onChange={setDataInput} placeholder ="price"></input>
+                    </div>
+                <button className="add" onClick ={addProducts}>ADD</button>
+                <button className="edit" onClick ={editProducts}>EDIT</button>
+            </form>
          </div>
          
     );
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -8,24 +8,28 @@ const Table = ({productos,deleteId, ...props}) => {
     <div>
         <h1 className="title">Lista de productos</h1>
         <table className ="table">
-            <tr className ="filas">
-                <th className ="fila">id</th>
-                <th className ="fila">name</th>
-                <th className ="fila">make</th>
-                <th className ="fila">price</th>
-                <th className ="fila">option</th>
-            </tr>
+            <thead>
+                <tr className ="filas">
+                    <th className ="fila">id</th>
+                    <th className ="fila">name</th>
+                    <th className ="fila">make</th>
+                    <th className ="fila">price</th>
+                    <th className ="fila">option</th>
+                </tr>
+            </thead>
+            <tbody>
             {productos.map(producto => {
                 return(
-                    <tr>
+                    <tr key={producto.id}>
                         <td>{producto.id}</td>
                         <td>{producto.name}</td>
                         <td>{producto.make}</td>
                         <td>{producto.price}</td>
                         <td><button onClick={()=> deleteId(producto.id)} className="eliminar">{producto.eliminar}Delete</button></td>
-                    </tr>
+                    </tr>                    
                 )
             })}
+            </tbody>
         </table>
     </div>
     );
